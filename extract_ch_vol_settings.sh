6d663dd72dad5daef69ffdfb894d577d3d9b74ab
a.mantoni <925171+eadmaster@users.noreply.github.com>
2022-12-16 00:38:32 +0100

    scripted commit

--- extract_ch_vol_settings.sh
+++ extract_ch_vol_settings.sh
@@ -1,16 +1,20 @@
 
 DIRNAME="$(basename "$PWD")"
 
-if [ ! -f "$DIRNAME.opt" ]; then
-	echo "$0 error: $DIRNAME.opt not found. This script must be invoked from a core config dir."
-	exit 1
-fi
+#if [ ! -f "$DIRNAME.opt" ]; then
+	#echo "$0 error: $DIRNAME.opt not found. This script must be invoked from a core config dir."
+	#exit 1
+#fi
+
+#TODO: create copies with nointro names via  "rengoodnes2nointro=renfromdats.py --rename  \"$(getgoodtoolsdatfilepath NES)\" \"$(getnointrodatfilepath - Nintendo Entertainment System)\""
 
 for f in *.opt; do 
 	[ "$f" = "$DIRNAME.opt" ] && continue  # skip default core options
 	mv "$f" "$f.bak"
-	grep "_volume" "$f.bak" > "$f"
+	grep "vol" "$f.bak" > "$f"
 	rm "$f.bak"
 	echo "#include \"$DIRNAME.opt\"" >> "$f"
+	
+	
 done
 
